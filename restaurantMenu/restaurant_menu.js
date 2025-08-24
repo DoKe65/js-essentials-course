0eb7ade1b857fa47d8d7ce5d5061e268502d14fb
DoKe65 <aurelia65@gmail.com>
2025-08-24 08:07:13 -0400

    Added Employees Details display

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata'];
+const mainCourseMenu = ['Steak - $24.90', 'Pasta - $19.50', 'Burger - $15.20', 'Salmon - $24.30'];
+const dessertMenu = ['Cake - $4.50', 'Ice Cream - $6.70', 'Pudding - $3.10', 'Fruit Salad - $3.85'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index+1}: ${item}</p>`).join(" ");
 document.getElementById("breakfastMenuItems").innerHTML = breakfastMenuItemsHTML;
