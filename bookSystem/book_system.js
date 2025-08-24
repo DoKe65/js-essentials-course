f7b6a16e6925abbcc835122cfa8a693f3ed65803
DoKe65 <aurelia65@gmail.com>
2025-08-24 11:20:39 -0400

    Added delete function to books display

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -27,7 +27,8 @@ function showbooks() {
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
         <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
-        <button onclick="editbook(${index})">Edit</button>`
+        <button onclick="editbook(${index})">Edit</button>
+        <button onclick="deletebook(${index})">Delete</button>`
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -42,9 +43,17 @@ function editbook(index) {
     showbooks(); // Refresh list
 }
 
+function deletebook(index) {
+    const book = books[index];
+    const indexOfBook = books.indexOf(book);
+    books.splice(indexOfBook, 1);
+    showbooks();
+}
+
 function clearInputs() {
     document.getElementById('bookName').value = '';
     document.getElementById('authorName').value = '';
     document.getElementById('bookDescription').value = '';
     document.getElementById('pagesNumber').value = '';
 }
+
